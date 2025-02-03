7014d3baf3247872cde89d40be69308fbb1c2673
JoshSimonMCGD <josh.j.simon@gmail.com>
2025-02-03 11:56:09 -0500

    Final Version

--- obj/Debug/net9.0/Assignment1JoshSimon.AssemblyInfo.cs
+++ obj/Debug/net9.0/Assignment1JoshSimon.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment1JoshSimon")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e59e0df628f5ca0a2608c026e631308149b9ba9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment1JoshSimon")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment1JoshSimon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
